20286801383de79fe49e79a8a725968e2feb8023
yiming543 <yiming.shi@true-win.com>
2026-03-05 10:39:57 +0800

    V04
    //20260304 V04 CS:5703
    // 增加WELCOME燈控制(unlock時輸出WELCOME_PIN)

---
+++ TY1500_U2_V04.X/_build/TY1500_U2_V01/default/CMakeFiles/4.0.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26100")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26100")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "")
+
+include("D:/yiming/00work/TY1500_U2_V01.X/cmake/TY1500_U2_V01/default/.generated/toolchain.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ TY1500_U2_V04.X/build/default/production/LED_output.p1.d
@@ -0,0 +1,4 @@
+build/default/production/LED_output.p1:  \
+LED_output.c  \
+LED_output.h  \
+mcc_generated_files/pin_manager.h 
